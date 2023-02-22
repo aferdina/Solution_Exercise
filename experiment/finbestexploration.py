21ca8a632398c44ae59da1fc75ae93aaa61084cf
aferdina <aferdina@mail.uni-mannheim>
2023-02-22 18:53:29 +0000

    Update from https://github.com/aferdina/SolutionExercisePrivate/commit/01ee1d874966d01973123f8e6bda6d308231f7f2

---
+++ experiment/finbestexploration.py
@@ -0,0 +1,77 @@
+from sheet01.experiment.trainingsutils import bound_function
+import numpy as np
+from sheet01.environments.multiarmed_bandits import GaussianBanditEnv
+from scipy.optimize import minimize_scalar
+from sheet01.models.explorethencommit import ExploreThenCommit
+from sheet01.experiment.run_explorethencommit import train_exandcommit
+import math
+import matplotlib.pyplot as plt
+
+NUM_GAMES = 5000
+MAX_STEPS = 10000
+N_ARMS = 10
+
+
+def explorethencommit_optim(max_steps, n_arms, num_games, printed):
+
+    rewards = np.zeros(shape=(num_games, max_steps))
+    regrets = np.zeros(shape=(num_games, max_steps))
+    optimalities = np.zeros(shape=(num_games, max_steps))
+    for game in range(num_games):
+        mean_parameter = np.random.normal(
+            loc=0.0, scale=1.0, size=n_arms).tolist()
+        env = GaussianBanditEnv(
+            mean_parameter=mean_parameter, max_steps=max_steps)
+        deltas = np.max(mean_parameter)-mean_parameter
+        upperbound = max_steps/n_arms
+        bounds = [1, upperbound]
+        res = minimize_scalar(lambda x_explore: bound_function(
+            explore=x_explore, deltas=deltas, number_of_games=max_steps), bounds=bounds)
+        if res.success:
+            explore = math.ceil(res.x)
+            # print("Optimal value: ", explore)
+        else:
+            # print("Optimization failed: ", res.message)
+            raise KeyError
+        agent = ExploreThenCommit(explore=explore, n_arms=n_arms)
+        reward, _chosen_arms, regret, optimality = train_exandcommit(
+            agent=agent, env=env, num_games=1, parameter="explore", printed=False)
+        rewards[game,] = reward
+        regrets[game,] = regret
+        optimalities[game,] = optimality
+
+    mean_rewards = np.mean(rewards, axis=0)
+    mean_cum_rewards = np.cumsum(mean_rewards,axis=0)
+    mean_regrets = np.mean(regrets, axis=0)
+    mean_optimalities = np.mean(optimalities, axis=0)
+    index_array = np.arange(len(mean_optimalities))
+    mean_optimalities = mean_optimalities / (index_array + 1)
+
+    print(f"total mean reward of optim is {mean_cum_rewards[-1]}")
+    print(f"total mean regret of optim is {mean_regrets[-1]}")
+    print(f"total number of optimal action of optim is {mean_optimalities[-1]}")
+    if printed:
+        plt.subplot(4, 1, 1)
+        plt.plot(range(len(mean_rewards)), mean_rewards,
+                 label=f"mean reward optim m")
+        plt.legend()
+        plt.subplot(4, 1, 2)
+        plt.plot(range(len(mean_cum_rewards)), mean_cum_rewards,
+                 label=f"cumsum reward optim m")
+        plt.legend()
+        plt.subplot(4, 1, 3)
+        plt.plot(range(len(mean_regrets)), mean_regrets,
+                 label=f"regrets optim m")
+        plt.legend()
+        plt.subplot(4, 1, 4)
+        plt.plot(range(len(mean_optimalities)), mean_optimalities,
+                 label=f"optimalities optim m")
+        plt.legend()
+        plt.show()
+
+    return mean_rewards, mean_cum_rewards, mean_regrets, mean_optimalities
+
+
+if __name__ == "__main__":
+    explorethencommit_optim(max_steps=MAX_STEPS,
+                            n_arms=N_ARMS, num_games=NUM_GAMES, printed=True)
---
+++ models/explorethencommit.py
@@ -0,0 +1,55 @@
+
+class ExploreThenCommit():
+    """ explore then commit algorithm
+    """
+
+    def __init__(self, explore, n_arms):
+        """initialize explore then commit algorithm
+
+        Args:
+            explore (int): number of steps to explore each arm
+            n_arms (int): number of arms in the multi arm bandit
+        """
+        self.explore = explore
+        self.counts_actions = [0 for _ in range(n_arms)]
+        self.values = [0.0 for _ in range(n_arms)]
+        self.n_arms = n_arms
+
+    def select_arm(self, count):
+        """ select the best arm given the estimators of the values
+
+        Args:
+            count (int): step in the game
+
+        Returns:
+            int: best action based on the estimators of the values
+        """
+        if self.explore*self.n_arms < count:
+            max_value = max(self.values)
+            best_action = (self.values).index(max_value)
+            return best_action
+        return count % self.n_arms
+
+    def update(self, chosen_arm, reward):
+        """ update the value estimators and counts based on the new observed
+         reward and played action
+
+        Args:
+            chosen_arm (int): action which was played
+            reward (float): reward of the multiarmed bandit, based on playing action `chosen_arm`
+        """
+        # increment the chosen arm
+        self.counts_actions[chosen_arm] = self.counts_actions[chosen_arm] + 1
+        times_played_chosen_arm = self.counts_actions[chosen_arm]
+        value = self.values[chosen_arm]
+        # update via memory trick
+        new_value = ((times_played_chosen_arm - 1) / float(times_played_chosen_arm)
+                     ) * value + (1 / float(times_played_chosen_arm)) * reward
+        self.values[chosen_arm] = new_value
+
+    def reset(self):
+        """ reset agent by resetting all required statistics
+        """
+        self.counts_actions = [0 for _ in range(self.n_arms)]
+        self.values = [0.0 for _ in range(self.n_arms)]
+
